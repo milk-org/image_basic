e7b0d0ed5635ca487930deeb498041f06070b75b
oguyon <oliv.guyon@gmail.com>
2020-04-11 15:59:05 -1000

    register functions

--- image_basic.c
+++ image_basic.c
@@ -402,182 +402,182 @@ static errno_t init_module_CLI()
         "imexpand im1 im2 2 2",
         "long basic_expand(const char *ID_name, const char *ID_name_out, int n1, int n2)");
 
-    /*
-        RegisterCLIcommand(
-            "imexpand3D",
-            __FILE__,
-            image_basic_expand3D_cli,
-            "expand 3D image",
-            "<image in> <output image> <x factor> <y factor> <z factor>",
-            "imexpand3D im1 im2 2 2 2",
-            "long basic_expand3D(const char *ID_name, const char *ID_name_out, int n1, int n2, int n3)");
+
+    RegisterCLIcommand(
+        "imexpand3D",
+        __FILE__,
+        image_basic_expand3D_cli,
+        "expand 3D image",
+        "<image in> <output image> <x factor> <y factor> <z factor>",
+        "imexpand3D im1 im2 2 2 2",
+        "long basic_expand3D(const char *ID_name, const char *ID_name_out, int n1, int n2, int n3)");
 
 
-        RegisterCLIcommand(
-            "resizeim",
-            __FILE__,
-            image_basic_resize_cli,
-            "resize 2D image",
-            "<image in> <output image> <new x size> <new y size>",
-            "resizeim im1 im2 230 200",
+    RegisterCLIcommand(
+        "resizeim",
+        __FILE__,
+        image_basic_resize_cli,
+        "resize 2D image",
+        "<image in> <output image> <new x size> <new y size>",
+        "resizeim im1 im2 230 200",
 
-            "long basic_resizeim(const char *imname_in, const char *imname_out, long xsizeout, long ysizeout)");
-
-
-        RegisterCLIcommand(
-            "rotateim",
-            __FILE__,
-            image_basic_rotate_cli,
-            "rotate 2D image",
-            "<image in> <output image> <angle>",
-            "rotateim imin imout 230",
-            "long basic_rotate(const char *ID_name, const char *ID_out_name, float angle)");
+        "long basic_resizeim(const char *imname_in, const char *imname_out, long xsizeout, long ysizeout)");
 
 
-
-        RegisterCLIcommand(
-            "im3Dto2D",
-            __FILE__,
-            image_basic_3Dto2D_cli,
-            "collapse first 2 axis of 3D image (in place)",
-            "<image name>",
-            "im3Dto2D im1",
-            "long image_basic_3Dto2D(const char *IDname)");
-
-
-        RegisterCLIcommand(
-            "imswapaxis2D",
-            __FILE__,
-            image_basic_SwapAxis2D_cli,
-            "Swap axis of a 2D image",
-            "<input image> <output image>",
-            "imswapaxis2D im1 im2",
-            "long image_basic_SwapAxis2D(const char *IDin_name, const char *IDout_name)");
-
-
-        RegisterCLIcommand(
-            "addim",
-            __FILE__,
-            image_basic_add_cli,
-            "add two 2D images of different size",
-            "<im1> <im2> <outim> <offsetx> <offsety>",
-            "addim im1 im2 outim 23 201",
-            "long basic_add(const char *ID_name1, const char *ID_name2, const char *ID_name_out, long off1, long off2)");
-
-
-        RegisterCLIcommand(
-            "addim3D",
-            __FILE__,
-            image_basic_add3D_cli,
-            "add two 3D images of different size",
-            "<im1> <im2> <outim> <offsetx> <offsety> <offsetz>",
-            "addim3D im1 im2 outim 23 201 0",
-            "long basic_add3D(const char *ID_name1, const char *ID_name2, const char *ID_name_out, long off1, long off2, long off3)");
+    RegisterCLIcommand(
+        "rotateim",
+        __FILE__,
+        image_basic_rotate_cli,
+        "rotate 2D image",
+        "<image in> <output image> <angle>",
+        "rotateim imin imout 230",
+        "long basic_rotate(const char *ID_name, const char *ID_out_name, float angle)");
 
 
 
-        RegisterCLIcommand(
-            "imcontract3D",
-            __FILE__,
-            image_basic_contract3D_cli,
-            "image binning (3D)",
-            "<inim> <outim> <binx> <biny> <binz>",
-            "imcontracteD im1 outim 4 4 1",
-            "long basic_contract3D(const char *ID_name, const char *ID_name_out, int n1, int n2, int n3)");
+    RegisterCLIcommand(
+        "im3Dto2D",
+        __FILE__,
+        image_basic_3Dto2D_cli,
+        "collapse first 2 axis of 3D image (in place)",
+        "<image name>",
+        "im3Dto2D im1",
+        "long image_basic_3Dto2D(const char *IDname)");
 
 
-        RegisterCLIcommand(
-            "imcontract",
-            __FILE__,
-            image_basic_contract_cli,
-            "image binning",
-            "<inim> <outim> <binx> <biny>",
-            "imcontract im1 outim 4 4",
-            "long basic_contract(const char *ID_name, const char *ID_name_out, int n1, int n2)");
+    RegisterCLIcommand(
+        "imswapaxis2D",
+        __FILE__,
+        image_basic_SwapAxis2D_cli,
+        "Swap axis of a 2D image",
+        "<input image> <output image>",
+        "imswapaxis2D im1 im2",
+        "long image_basic_SwapAxis2D(const char *IDin_name, const char *IDout_name)");
 
 
-        RegisterCLIcommand(
-            "imgetcircassym",
-            __FILE__,
-            IMAGE_BASIC_get_assym_component_cli,
-            "extract non-circular symmetric part of image",
-            "<inim> <outim> <xcenter> <ycenter>",
-            "imcgetcircassym imin imout 256.0 230.5",
-            "long IMAGE_BASIC_get_assym_component(const char *ID_name, const char *ID_out_name, float xcenter, float ycenter, const char *options)");
+    RegisterCLIcommand(
+        "addim",
+        __FILE__,
+        image_basic_add_cli,
+        "add two 2D images of different size",
+        "<im1> <im2> <outim> <offsetx> <offsety>",
+        "addim im1 im2 outim 23 201",
+        "long basic_add(const char *ID_name1, const char *ID_name2, const char *ID_name_out, long off1, long off2)");
 
 
-        RegisterCLIcommand(
-            "imgetcircsym",
-            __FILE__,
-            IMAGE_BASIC_get_sym_component_cli,
-            "extract circular symmetric part of image",
-            "<inim> <outim> <xcenter> <ycenter>",
-            "imcgetcircsym imin imout 256.0 230.5",
-            "long IMAGE_BASIC_get_sym_component(const char *ID_name, const char *ID_out_name, float xcenter, float ycenter)");
-
-
-
-        RegisterCLIcommand(
-            "loadfitsimgcube",
-            __FILE__,
-            image_basic_load_fitsimages_cube_cli,
-            "load multiple images into a single cube",
-            "loadfitsimgcube <string pattern> <outputcube>",
-            "loadfitsimgcube im out",
-            "long load_fitsimages_cube(const char *strfilter, const char *ID_out_name)");
+    RegisterCLIcommand(
+        "addim3D",
+        __FILE__,
+        image_basic_add3D_cli,
+        "add two 3D images of different size",
+        "<im1> <im2> <outim> <offsetx> <offsety> <offsetz>",
+        "addim3D im1 im2 outim 23 201 0",
+        "long basic_add3D(const char *ID_name1, const char *ID_name2, const char *ID_name_out, long off1, long off2, long off3)");
 
 
 
-        RegisterCLIcommand(
-            "cubecollapse",
-            __FILE__,
-            image_basic_cubecollapse_cli,
-            "collapse a cube along z",
-            "cubecollapse <inim> <outim>",
-            "cubecollapse im1 outim",
-            "long cube_collapse(const char *ID_in_name, const char *ID_out_name)");
+    RegisterCLIcommand(
+        "imcontract3D",
+        __FILE__,
+        image_basic_contract3D_cli,
+        "image binning (3D)",
+        "<inim> <outim> <binx> <biny> <binz>",
+        "imcontracteD im1 outim 4 4 1",
+        "long basic_contract3D(const char *ID_name, const char *ID_name_out, int n1, int n2, int n3)");
 
 
-        RegisterCLIcommand(
-            "imindexmap",
-            __FILE__,
-            image_basic_indexmap_cli,
-            "map input values to output image unsing index map",
-            "imindexmap <indexmap> <values> <output>",
-            "imindexmap imap imval outmap",
-            "long image_basic_indexmap(char *ID_index_name, char *ID_values_name, char *IDout_name)");
+    RegisterCLIcommand(
+        "imcontract",
+        __FILE__,
+        image_basic_contract_cli,
+        "image binning",
+        "<inim> <outim> <binx> <biny>",
+        "imcontract im1 outim 4 4",
+        "long basic_contract(const char *ID_name, const char *ID_name_out, int n1, int n2)");
 
 
-        RegisterCLIcommand(
-            "imgstreamave",
-            __FILE__,
-            image_basic_streamaverage_cli,
-            "average stream of images",
-            "imgstreamave <imin> <NBcoadd [long]> <imout> <mode> <semindex>",
-            "imgstreamave im 100 imave 0 -1",
-            "long IMAGE_BASIC_streamaverage(const char *IDname, long NBcoadd, const char *IDoutname, int mode, int semindex)");
+    RegisterCLIcommand(
+        "imgetcircassym",
+        __FILE__,
+        IMAGE_BASIC_get_assym_component_cli,
+        "extract non-circular symmetric part of image",
+        "<inim> <outim> <xcenter> <ycenter>",
+        "imcgetcircassym imin imout 256.0 230.5",
+        "long IMAGE_BASIC_get_assym_component(const char *ID_name, const char *ID_out_name, float xcenter, float ycenter, const char *options)");
 
 
-        RegisterCLIcommand(
-            "imgstreamfeed",
-            __FILE__,
-            image_basic_streamfeed_cli,
-            "feed stream of images",
-            "<input image/cube> <stream> <fequ [Hz]>",
-            "imgstreamfeed im imstream 100",
-            "long IMAGE_BASIC_streamfeed(const char *IDname, const char *streamname, float frequ)");
+    RegisterCLIcommand(
+        "imgetcircsym",
+        __FILE__,
+        IMAGE_BASIC_get_sym_component_cli,
+        "extract circular symmetric part of image",
+        "<inim> <outim> <xcenter> <ycenter>",
+        "imcgetcircsym imin imout 256.0 230.5",
+        "long IMAGE_BASIC_get_sym_component(const char *ID_name, const char *ID_out_name, float xcenter, float ycenter)");
 
 
-        RegisterCLIcommand(
-            "imgstreamrec",
-            __FILE__,
-            IMAGE_BASIC_streamrecord_cli,
-            "record stream of images",
-            "<stream> <# frames> <output>",
-            "imgstreamrec imstream 100 imrec",
-            "long IMAGE_BASIC_streamrecord(const char *streamname, long NBframes, const char *IDname)");
 
-    */
+    RegisterCLIcommand(
+        "loadfitsimgcube",
+        __FILE__,
+        image_basic_load_fitsimages_cube_cli,
+        "load multiple images into a single cube",
+        "loadfitsimgcube <string pattern> <outputcube>",
+        "loadfitsimgcube im out",
+        "long load_fitsimages_cube(const char *strfilter, const char *ID_out_name)");
+
+
+
+    RegisterCLIcommand(
+        "cubecollapse",
+        __FILE__,
+        image_basic_cubecollapse_cli,
+        "collapse a cube along z",
+        "cubecollapse <inim> <outim>",
+        "cubecollapse im1 outim",
+        "long cube_collapse(const char *ID_in_name, const char *ID_out_name)");
+
+
+    RegisterCLIcommand(
+        "imindexmap",
+        __FILE__,
+        image_basic_indexmap_cli,
+        "map input values to output image unsing index map",
+        "imindexmap <indexmap> <values> <output>",
+        "imindexmap imap imval outmap",
+        "long image_basic_indexmap(char *ID_index_name, char *ID_values_name, char *IDout_name)");
+
+
+    RegisterCLIcommand(
+        "imgstreamave",
+        __FILE__,
+        image_basic_streamaverage_cli,
+        "average stream of images",
+        "imgstreamave <imin> <NBcoadd [long]> <imout> <mode> <semindex>",
+        "imgstreamave im 100 imave 0 -1",
+        "long IMAGE_BASIC_streamaverage(const char *IDname, long NBcoadd, const char *IDoutname, int mode, int semindex)");
+
+
+    RegisterCLIcommand(
+        "imgstreamfeed",
+        __FILE__,
+        image_basic_streamfeed_cli,
+        "feed stream of images",
+        "<input image/cube> <stream> <fequ [Hz]>",
+        "imgstreamfeed im imstream 100",
+        "long IMAGE_BASIC_streamfeed(const char *IDname, const char *streamname, float frequ)");
+
+
+    RegisterCLIcommand(
+        "imgstreamrec",
+        __FILE__,
+        IMAGE_BASIC_streamrecord_cli,
+        "record stream of images",
+        "<stream> <# frames> <output>",
+        "imgstreamrec imstream 100 imrec",
+        "long IMAGE_BASIC_streamrecord(const char *streamname, long NBframes, const char *IDname)");
+
+
 
     // add atexit functions here
 
